ff68562fb3472c7cd44c2cb76ef2f216acb99186
Joachim Gonvers <joachim.gonvers@gmail.com>
2023-04-04 10:45:20 +0200

    first attempt for 77 using recursion

--- 77. combinations/solution.py
+++ 77. combinations/solution.py
@@ -1,3 +1,17 @@
+#from itertools import combinations
+#class Solution:
+#    def combine(self, n: int, k: int) -> List[List[int]]:
+#        return list(combinations(list(range(1,n+1)), k))
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        
+        if n == 1: return [[1]]
+        if k == 1: return [[x] for x in range(1,n+1)]
+        sol = []
+        for x in range(n,0,-1):
+            less = self.combine(x-1, k-1)
+            for e in less:
+                e.append(x)
+            sol.extend(less)
+        return sol
+            
