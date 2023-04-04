375e93931f93ae43ffb5ca70e2adb30210555390
Joachim Gonvers <joachim.gonvers@gmail.com>
2023-04-04 10:48:15 +0200

    2nd attempt for 77 using recursion

--- 77. combinations/solution.py
+++ 77. combinations/solution.py
@@ -5,8 +5,8 @@
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        if n == 1: return [[1]]
         if k == 1: return [[x] for x in range(1,n+1)]
+        if k == n: return [list(range(1,n+1))]
         sol = []
         for x in range(n,0,-1):
             less = self.combine(x-1, k-1)
@@ -14,4 +14,3 @@ class Solution:
                 e.append(x)
             sol.extend(less)
         return sol
-            
