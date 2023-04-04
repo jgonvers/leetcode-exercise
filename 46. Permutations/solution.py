424f1da9469231d23d9a90edfef73857ce97318a
Joachim Gonvers <joachim.gonvers@gmail.com>
2023-04-04 10:56:07 +0200

    1st attenmpt for 46

--- 46. Permutations/solution.py
+++ 46. Permutations/solution.py
@@ -1,3 +1,11 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        
+        if len(nums) == 1: return [nums]
+        sol = []
+        for i in range(len(nums)):
+            less = self.permute(nums[:i]+nums[i+1:])
+            for e in less:
+                e.append(nums[i])
+            sol.extend(less)
+        return sol
+            
