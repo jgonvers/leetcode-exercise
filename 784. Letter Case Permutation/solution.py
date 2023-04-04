05f0597b1eeb4a1836f0b13d158730a1cb24e273
Joachim Gonvers <joachim.gonvers@gmail.com>
2023-04-04 11:06:51 +0200

    1s attempt 784

--- 784. Letter Case Permutation/solution.py
+++ 784. Letter Case Permutation/solution.py
@@ -1,3 +1,15 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        
+        def possibilities(char):
+            if char in string.digits:
+                return [char]
+            return [char.lower(), char.upper()]
+        if len(s) == 1: return possibilities(s)
+        cur_pos = possibilities(s[0])
+        pos = self.letterCasePermutation(s[1:])
+        sol = []
+        for x in pos:
+            for y in cur_pos:
+                sol.append(y+x)
+        return sol
+    
